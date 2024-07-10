7c8763c96c71a78c8ec2b579c40a548f90b6ec25
hyppolite <hyppolitak@gmail.com>
2024-07-10 03:02:50 +0100

    Create ExpenseEntryItemForm component

---
+++ src/components/ExpenseEntryItemForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ExpenseEntryItemForm extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Expense form</h1>
+            </div>
+        );
+    }
+}
+
+export default ExpenseEntryItemForm;
