fe121387c18e6248c0877d13fceff4b4f140257f
hyppolite <hyppolitak@gmail.com>
2024-07-10 03:16:04 +0100

    Configure Router in the App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Routes,
+  Route
+} from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import ExpenseEntryItemList from './components/ExpenseEntryItemList';
+import ExpenseEntryItemForm from './components/ExpenseEntryItemForm';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello Folks!</h1>
-        <p>Let us build an expense manager React application</p>
-      </div>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/list">List Expenses</Link>
+            </li>
+            <li>
+              <Link to="/add">Add Expense</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/list" element={<ExpenseEntryItemList />} />
+          <Route path="/add" element={<ExpenseEntryItemForm />} />
+        </Routes>
+      </Router>
     );
   }
 }
